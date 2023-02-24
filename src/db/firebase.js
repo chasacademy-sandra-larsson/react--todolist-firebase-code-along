99e15d4c7c67d0f23a7c2dce6cef859e685c0164
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2023-02-24 11:15:57 +0100

    Update firebase.js

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -6,12 +6,6 @@ import {
 
 const firebaseConfig = {
  // Din config
- apiKey: "AIzaSyC5iHdVpGijRdGYI_9bqinncKGtvtuzze8",
- authDomain: "todo-list-react-24-2.firebaseapp.com",
- projectId: "todo-list-react-24-2",
- storageBucket: "todo-list-react-24-2.appspot.com",
- messagingSenderId: "315184499628",
- appId: "1:315184499628:web:74f2ed23d3a5b7f4bd0809"
 };
 
 const app = initializeApp(firebaseConfig);
